2073e167227496eac7d0eadce78bec77a75cd2c3
Connorsan <jacob.connors3@gmail.com>
2014-03-10 22:30:35 -0500

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WorkoutApp::Application.config.secret_key_base = '324126c9c581265a4ed026bea902190c35f7f4f2d77e5de87865a36ec334f897a0a9a53ec1d043745cc7f64bb58f2ea7af753afa1811856e093639a3f3adc45f'
